8704962afb243dc59f669b8be2416f9f3a9f0005
Daniel González Briseño <qbixmex@gmail.com>
2025-10-16 17:45:15 -0600

    feature: add team management pages and components
    - Created TeamPage for displaying team details.
    - Added CreateUser page for creating new teams.
    - Implemented EditTeam page for editing existing teams.
    - Developed TeamsPage for listing all teams with actions.
    - Introduced app sidebar navigation for teams, tournaments, and coaches.
    - Added custom icons for game score and soccer field.
    - Defined interfaces for Team, Tournament, User, and Cloudinary responses.
    - Implemented pagination interface.
    - Updated nav-main component to include new routes.

---
+++ src/app/admin/entrenadores/crear/page.tsx
@@ -0,0 +1,36 @@
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { TeamForm } from "../(components)/teamForm";
+import { Session } from "next-auth";
+import { auth } from "@/auth.config";
+import { redirect } from "next/navigation";
+
+export const CreateUser = async () => {
+  const session = await auth();
+
+  if (!session?.user.roles.includes('admin')) {
+    const message = '¡ No tienes permisos administrativos para crear usuarios !';
+    redirect(`/admin/usuarios?error=${encodeURIComponent(message)}`);
+  }
+
+  return (
+    <div className="flex flex-1 flex-col gap-5 p-5 pt-0">
+      <div className="bg-muted/50 min-h-[100vh] flex-1 flex rounded-xl md:min-h-min p-10">
+        <Card className="w-full shadow-none bg-neutral-100 dark:bg-linear-to-br dark:from-zinc-950 dark:to-zinc-800">
+          <CardHeader className="flex items-center justify-between">
+            <CardTitle>Crear Equipo</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <TeamForm session={session as Session} />
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  );
+};
+
+export default CreateUser;
--- src/components/app-sidebar/index.tsx
+++ src/components/app-sidebar/index.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { ComponentProps, FC } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import {
@@ -12,83 +13,10 @@ import {
   SidebarMenuItem,
   SidebarRail,
 } from "@/components/ui/sidebar";
-import {
-  Users,
-  Globe,
-  ListIcon,
-  PlusIcon,
-  Flag,
-  Trophy,
-} from "lucide-react";
 
-import { type NavItem, NavMain } from "@/components/nav-main";
-import { type NavLink, NavLinks } from "@/components/nav-links";
-import { ComponentProps, FC } from "react";
-
-const navMain: NavItem[] = [
-  {
-    label: "Usuarios",
-    url: "#",
-    icon: Users,
-    isActive: false,
-    subItems: [
-      {
-        label: "Lista",
-        url: "/admin/usuarios",
-        icon: ListIcon,
-      },
-      {
-        label: "Crear",
-        url: "/admin/usuarios/crear",
-        icon: PlusIcon,
-      },
-    ],
-  },
-  {
-    label: "Equipos",
-    url: "#",
-    icon: Flag,
-    isActive: false,
-    subItems: [
-      {
-        label: "Lista",
-        url: "/admin/equipos",
-        icon: ListIcon,
-      },
-      {
-        label: "Crear",
-        url: "/admin/equipos/crear",
-        icon: PlusIcon,
-      },
-    ],
-  },
-  {
-    label: "Torneos",
-    url: "#",
-    icon: Trophy,
-    isActive: false,
-    subItems: [
-      {
-        label: "Lista",
-        url: "/admin/torneos",
-        icon: ListIcon,
-      },
-      {
-        label: "Crear",
-        url: "/admin/torneos/crear",
-        icon: PlusIcon,
-      },
-    ],
-  },
-];
-
-const navLinks: NavLink[] = [
-  {
-    label: "Página Principal",
-    url: "/",
-    icon: Globe,
-  },
-];
+import { navMain, navLinks } from "./data";
+import { NavMain } from "@/components/nav-main";
+import { NavLinks } from "@/components/nav-links";
 
 type Props = Readonly<ComponentProps<typeof Sidebar>>;
 
@@ -126,7 +54,7 @@ export const AppSidebar: FC<Props> = ({ ...props }) => {
       </SidebarContent>
 
       <SidebarFooter>
-        
+        {/* EMPTY FOR NOW */}
       </SidebarFooter>
 
       <SidebarRail />
