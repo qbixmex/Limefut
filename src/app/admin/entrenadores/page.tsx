54b32092e24907b61ecb475ef3983e1e13bead0f
Daniel González Briseño <qbixmex@gmail.com>
2025-11-06 11:47:59 -0600

    Implements coaches pagination and search
    Adds pagination to the coaches list in the admin panel.
    
    Implements a search functionality to filter coaches by name, email and phone.
    
    Creates a skeleton component for the coaches table to improve loading experience.
    
    Refactors the coaches list page into a table component.

--- src/app/admin/entrenadores/page.tsx
+++ src/app/admin/entrenadores/page.tsx
@@ -1,39 +1,29 @@
-import Image from "next/image";
+import { Suspense, type FC } from 'react';
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
 import {
   Tooltip,
   TooltipContent,
   TooltipTrigger,
 } from "@/components/ui/tooltip";
-import {
-  Check,
-  CircleOff,
-  Pencil,
-  InfoIcon,
-  Plus,
-} from "lucide-react";
-import { GiWhistle } from "react-icons/gi";
-import { Badge } from "@/components/ui/badge";
+import { Plus } from "lucide-react";
 import Link from "next/link";
-import { DeleteCoach } from "./(components)/delete-coach";
-import { fetchCoachesAction } from "./(actions)";
 import { ErrorHandler } from "@/shared/components/errorHandler";
-import { auth } from "@/auth.config";
+import { CoachesTable } from './(components)/coaches-table';
+import { CoachesTableSkeleton } from './(components)/coaches-table-skeleton';
+import { Search } from '@/root/src/shared/components/search';
 
-export const CoachesPage = async () => {
-  const response = await fetchCoachesAction();
-  const coaches = response.coaches;
+type Props = Readonly<{
+  searchParams?: Promise<{
+    query?: string;
+    page?: string;
+  }>;
+}>;
 
-  const session = await auth();
+export const CoachesPage: FC<Props> = async (props) => {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || '';
+  const currentPage = Number(searchParams?.page) || 1;
 
   return (
     <>
@@ -43,7 +33,8 @@ export const CoachesPage = async () => {
           <Card className="w-full bg-linear-to-br from-zinc-100 to-zinc-50 dark:from-zinc-950 dark:to-zinc-800 shadow-none">
             <CardHeader className="flex items-center justify-between">
               <CardTitle>Lista de Entrenadores</CardTitle>
-              <div>
+              <section className="flex gap-5 items-center">
+                <Search placeholder="Buscar entrenador ..." />
                 <Tooltip>
                   <TooltipTrigger asChild>
                     <Link href="/admin/entrenadores/crear">
@@ -56,98 +47,15 @@ export const CoachesPage = async () => {
                     <p>crear</p>
                   </TooltipContent>
                 </Tooltip>
-              </div>
+              </section>
             </CardHeader>
             <CardContent>
-              {coaches && coaches.length > 0 ? (
-                <Table>
-                  <TableHeader>
-                    <TableRow>
-                      <TableHead className="w-[100px]">Imagen</TableHead>
-                      <TableHead>Nombre</TableHead>
-                      <TableHead>Correo Electrónico</TableHead>
-                      <TableHead>Teléfono</TableHead>
-                      <TableHead className="text-center">Equipos</TableHead>
-                      <TableHead className="text-center">Activo</TableHead>
-                      <TableHead>Acciones</TableHead>
-                    </TableRow>
-                  </TableHeader>
-                  <TableBody>
-                    {coaches.map((coach) => (
-                      <TableRow key={coach.id}>
-                        <TableCell>
-                          <Link href={`/admin/entrenadores/${coach.id}`}>
-                            {
-                              !coach.imageUrl ? (
-                                <figure className="bg-gray-800 size-[60px] rounded-xl flex items-center justify-center">
-                                  <GiWhistle size={35} className="text-gray-300" />
-                                </figure>
-                              ) : (
-                                <Image
-                                  src={coach.imageUrl}
-                                  alt={`${coach.name} picture`}
-                                  width={75}
-                                  height={75}
-                                  className="size-18 rounded-xl object-cover"
-                                />
-                              )
-                            }
-                          </Link>
-                        </TableCell>
-                        <TableCell>{coach.name}</TableCell>
-                        <TableCell>{coach.email}</TableCell>
-                        <TableCell>{coach.phone}</TableCell>
-                        <TableCell className="text-center">
-                          <Badge variant="outline-info">{coach.teamsCount}</Badge>
-                        </TableCell>
-                        <TableCell className="text-center">
-                          <Badge variant={coach.active ? 'outline-success' : 'outline-secondary'}>
-                            {coach.active ? <Check /> : <CircleOff />}
-                          </Badge>
-                        </TableCell>
-                        <TableCell>
-                          <div className="flex gap-3">
-                            <Tooltip>
-                              <TooltipTrigger asChild>
-                                <Link href={`/admin/entrenadores/perfil/${coach.id}`}>
-                                  <Button variant="outline-info" size="icon">
-                                    <InfoIcon />
-                                  </Button>
-                                </Link>
-                              </TooltipTrigger>
-                              <TooltipContent side="top">
-                                detalles
-                              </TooltipContent>
-                            </Tooltip>
-                            <Tooltip>
-                              <TooltipTrigger asChild>
-                                <Link href={`/admin/entrenadores/editar/${coach.id}`}>
-                                  <Button variant="outline-warning" size="icon">
-                                    <Pencil />
-                                  </Button>
-                                </Link>
-                              </TooltipTrigger>
-                              <TooltipContent side="top">
-                                <p>editar</p>
-                              </TooltipContent>
-                            </Tooltip>
-                            <DeleteCoach
-                              coachId={coach.id}
-                              roles={session?.user.roles as string[]}
-                            />
-                          </div>
-                        </TableCell>
-                      </TableRow>
-                    ))}
-                  </TableBody>
-                </Table>
-              ) : (
-                <div className="border border-sky-600 p-5 rounded">
-                  <p className="text-sky-500 text-center text-xl font-semibold">
-                    Todavía no hay entrenadores creados
-                  </p>
-                </div>
-              )}
+              <Suspense
+                key={`${query}-${currentPage}`}
+                fallback={<CoachesTableSkeleton colCount={7} rowCount={6} />}
+              >
+                <CoachesTable query={query} currentPage={currentPage} />
+              </Suspense>
             </CardContent>
           </Card>
         </div>
