f86f5681d0f55d93848b0cd6730b434affadf003
Daniel González Briseño <qbixmex@gmail.com>
2025-11-06 10:52:38 -0600

    Fixed path and Version Updated to 0.21.0

--- src/app/admin/usuarios/crear/page.tsx
+++ src/app/admin/usuarios/crear/page.tsx
@@ -4,7 +4,7 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { UserForm } from "../(components)/userForm";
+import { UsersForm } from "../(components)/usersForm";
 import type { Session } from "next-auth";
 import { auth } from "@/auth.config";
 import { redirect } from "next/navigation";
@@ -19,13 +19,13 @@ export const CreateUser = async () => {
 
   return (
     <div className="flex flex-1 flex-col gap-5 p-5 pt-0">
-      <div className="bg-muted/50 min-h-[100vh] flex-1 flex rounded-xl md:min-h-min p-10">
+      <div className="bg-muted/50 min-h-screen flex-1 flex rounded-xl md:min-h-min p-10">
         <Card className="w-full shadow-none bg-neutral-100 dark:bg-linear-to-br dark:from-zinc-950 dark:to-zinc-800">
           <CardHeader className="flex items-center justify-between">
             <CardTitle>Crear Usuario</CardTitle>
           </CardHeader>
           <CardContent>
-            <UserForm session={session as Session} />
+            <UsersForm session={session as Session} />
           </CardContent>
         </Card>
       </div>
