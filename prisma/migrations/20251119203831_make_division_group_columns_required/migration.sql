f86c19e4294985f9dee22c0b1e0c9009ed189eeb
Daniel González Briseño <qbixmex@gmail.com>
2025-11-19 14:43:26 -0600

    Updates Prisma and adds team page
    Updates Prisma to v7, includes adapter-pg.
    Also adds division and group columns to tournament model.
    Implements public teams page.

---
+++ prisma/migrations/20251119203831_make_division_group_columns_required/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE "tournaments" ALTER COLUMN "division" DROP DEFAULT,
+ALTER COLUMN "group" DROP DEFAULT;
--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from "../generated/prisma/client";
 import { withAccelerate } from '@prisma/extension-accelerate';
 
 const globalForPrisma = global as unknown as { 
-  prisma: PrismaClient
+  prisma: PrismaClient,
 };
 
 const prisma = globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate());
