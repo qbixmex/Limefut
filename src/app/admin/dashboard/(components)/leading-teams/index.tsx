2398e184a6c7573639060ec3766f6c9c581b50b0
Daniel González Briseño <qbixmex@gmail.com>
2025-12-25 11:52:58 -0600

    Implements the leading teams component for the admin dashboard.
    This component fetches and displays the top-ranked teams based on their points in active tournaments. Also implements a skeleton loader for better UX.
    
    It utilizes a new server action to retrieve the data and renders it in a table format, enhancing the dashboard's overview of team performance.

---
+++ src/app/admin/dashboard/(components)/leading-teams/index.tsx
@@ -0,0 +1,54 @@
+import { Badge } from "~/src/components/ui/badge";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "~/src/components/ui/table";
+import { fetchLeadingTeamsAction } from "../../(actions)/fetchLeadingTeamsAction";
+import Link from "next/link";
+
+export const LeadingTeams = async () => {
+  const { leadingTeams } = await fetchLeadingTeamsAction({ quantity: 100 });
+
+  return (
+    <>
+      <h2 className="text-emerald-500 text-2xl font-semibold mb-4">
+        Equipos Punteros
+      </h2>
+
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Equipo</TableHead>
+            <TableHead>Torneo</TableHead>
+            <TableHead className="text-center">Puntos</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {leadingTeams.map((leading) => (
+            <TableRow key={leading.team.id}>
+              <TableCell>
+                <Link
+                  href={`/admin/equipos/${leading.team.permalink}`}
+                  className="text-wrap">
+                  {leading.team.name}
+                </Link>
+              </TableCell>
+              <TableCell>
+                <TableCell>
+                  <Link
+                    href={`/admin/torneos/${leading.tournament.permalink}`}
+                    className="text-wrap"
+                  >
+                    {leading.tournament.name}
+                  </Link>
+                </TableCell>
+              </TableCell>
+              <TableCell className="text-center">
+                <Badge variant="outline-primary">{leading.points}</Badge>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </>
+  );
+};
+
+export default LeadingTeams;
