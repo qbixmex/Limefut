9645c71bb980e2a623ce5b10dd5215c7eae6c19f
Daniel González Briseño <qbixmex@gmail.com>
2025-12-30 10:59:09 -0600

    Improves match completion flow and UI
    Enhances the match completion process by adjusting the score visibility logic and repositioning the "Finalizar Encuentro" button.
    
    The score visibility is now updated based on the match status, ensuring it's hidden only when the match is explicitly completed.
    The "Finalizar Encuentro" button is moved to the top right corner for better UI.

--- src/app/admin/encuentros/crear/page.tsx
+++ src/app/admin/encuentros/crear/page.tsx
@@ -20,7 +20,7 @@ const CreateMatchPage: FC<Props> = async ({ searchParams }) => {
   return (
     <div className="flex flex-1 flex-col gap-5 p-5 pt-0">
       <div className="bg-muted/50 min-h-screen flex-1 flex rounded-xl md:min-h-min p-10">
-        <Card className="w-full shadow-none bg-neutral-100 dark:bg-linear-to-br dark:from-zinc-950 dark:to-zinc-800">
+        <Card className="w-full shadow-none bg-neutral-100 dark:bg-linear-to-br dark:from-zinc-950 dark:to-zinc-800 relative">
           <CardHeader className="flex items-center justify-between">
             <CardTitle>Crear Encuentro</CardTitle>
           </CardHeader>
