1dcd1abee0d8de95d08ab81445211e939bc1f20c
Daniel González Briseño <qbixmex@gmail.com>
2026-03-11 16:12:06 -0600

    Database URL Improvement
    In case is not defined throw new error otherwise use DATABASE_URL environment variable

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,9 +1,18 @@
 import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '../generated/prisma/client';
 
-const DATABASE_URL = process.env.DATABASE_URL ?? '';
+let databaseUrl = '';
 
-const adapter = new PrismaPg({ connectionString: DATABASE_URL });
+if (process.env.DATABASE_URL === null) {
+  throw new Error(
+    'DATABASE_URL is not present !, \n'
+    + 'be sure is in the environment variables',
+  );
+} else {
+  databaseUrl = process.env.DATABASE_URL;
+}
+
+const adapter = new PrismaPg({ connectionString: databaseUrl });
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
 
