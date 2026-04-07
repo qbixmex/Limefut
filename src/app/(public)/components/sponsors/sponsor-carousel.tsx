bbc70fd27e97167a178da80cfc3a96cd55204b9a
Daniel González Briseño <qbixmex@gmail.com>
2026-04-07 15:29:03 -0600

    Implemented sponsor click tracking functionality
    Adds a server action to increment sponsor click counts in the database and updates the SponsorCarousel component to trigger this action and redirect users when clicked. Also corrects the title on the sponsor edit page.

--- src/app/(public)/components/sponsors/sponsor-carousel.tsx
+++ src/app/(public)/components/sponsors/sponsor-carousel.tsx
@@ -4,6 +4,8 @@ import type { FC } from 'react';
 import { motion } from 'framer-motion';
 import Image from 'next/image';
 import { useSponsorsCarousel } from './use-sponsors-carousel';
+import { incrementClickAction } from '../../(actions)/videos/incrementClickAction';
+import type { SponsorType } from '../../(actions)/home/fetchPublicSponsorsAction';
 
 type Props = Readonly<{
   sponsors: {
@@ -15,27 +17,41 @@ type Props = Readonly<{
 }>;
 
 export const SponsorCarousel: FC<Props> = ({ sponsors = [], time = 10 }) => {
-  const { sponsor } = useSponsorsCarousel(sponsors, time);
+  const response = useSponsorsCarousel(sponsors, time);
 
   if (sponsors.length === 0) return null;
 
+  const sponsor = response.sponsor as SponsorType;
+
+  const handleSponsorClick = async (id: string) => {
+    await incrementClickAction(id);
+    if (sponsor.url && (sponsor.url.length > 0)) {
+      window.open(sponsor.url, '_blank');
+    }
+  };
+
   return (
-    <motion.figure
-      key={sponsor?.id ?? 'sponsor'}
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
-      transition={{ duration: 1, ease: 'easeInOut' }}
-      className="space-y-10"
-      aria-label={sponsor?.name ?? 'sponsor'}
+    <button
+      id="sponsor-button"
+      onClick={() => handleSponsorClick(sponsor.id)}
     >
-      <Image
-        width={0}
-        height={0}
-        src={sponsor?.imageUrl ?? ''}
-        alt={`Patrocinador ${sponsor?.name.toLowerCase() ?? 'Sponsor Image'}`}
-        className="w-full max-w-[288px] h-auto rounded"
-      />
-    </motion.figure>
+      <motion.figure
+        key={sponsor.id ?? 'sponsor'}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 1, ease: 'easeInOut' }}
+        className="space-y-10"
+        aria-label={sponsor.name ?? 'sponsor'}
+      >
+        <Image
+          width={0}
+          height={0}
+          src={sponsor.imageUrl ?? ''}
+          alt={`Patrocinador ${sponsor.name.toLowerCase() ?? 'Sponsor Image'}`}
+          className="w-full max-w-[288px] h-auto rounded"
+        />
+      </motion.figure>
+    </button>
   );
 };
