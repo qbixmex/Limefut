4bafade1818c609e0ba8ab12adab0a91c528b913
Daniel González <qbixmex@gmail.com>
2026-06-13 14:02:16 -0600

    Fixed match date switch
    If match date is set show the match date fields.

--- src/app/admin/liguilla/[playoff_id]/encuentros/(components)/form-fields/match-date-time/index.tsx
+++ src/app/admin/liguilla/[playoff_id]/encuentros/(components)/form-fields/match-date-time/index.tsx
@@ -18,9 +18,11 @@ import { es } from 'date-fns/locale';
 import { Switch } from '@/components/ui/switch';
 import { Label } from '@/components/ui/label';
 
-export const MatchDateTime: FC = () => {
+type Props = Readonly<{ isMatchDate: boolean; }>;
+
+export const MatchDateTime: FC<Props> = ({ isMatchDate }) => {
   const { control } = useFormContext();
-  const [enabledDate, setEnabledDate] = useState(false);
+  const [enabledDate, setEnabledDate] = useState(isMatchDate);
   const [open, setOpen] = useState(false);
 
   return (
