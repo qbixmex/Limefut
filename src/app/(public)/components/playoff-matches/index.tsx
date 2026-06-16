70cbf609c0b2ef8342fd0c03ff8634f0c109a8bb
Daniel González Briseño <qbixmex@gmail.com>
2026-06-16 15:04:46 -0600

    Added playoff match link to homepage

--- src/app/(public)/components/playoff-matches/index.tsx
+++ src/app/(public)/components/playoff-matches/index.tsx
@@ -35,57 +35,67 @@ export const PlayoffMatches: FC<Props> = async ({ playoffsPromise }) => {
         {(matches.length > 0) && (
           matches.map((match, index) => (
             <div key={match.id} className="flex flex-col gap-3 text-gray-800 dark:text-gray-200">
-              <div className="flex flex-col gap-5 md:flex-row md:gap-5">
-                <div className="w-full lg:w-1/2 order-2 lg:order-1">
-                  <MatchMetadata
-                    tournamentName={match.tournament.name}
-                    category={match.tournament.category}
-                    format={match.tournament.format}
-                    place={match.place}
-                    date={match.matchDate}
-                    status={match.status}
-                    round={match.round as ROUND_TYPE}
-                    group={match.group}
-                  />
-                </div>
-                <div className="w-full lg:w-1/2 grid grid-cols-3 order-1 lg:order-2">
-                  <Team
-                    imageUrl={match.localTeam.imageUrl}
-                    name={match.localTeam.name}
-                  />
-                  <div className="flex justify-center items-center gap-2">
-                    {match.penaltyShoots && (
-                      <span className="font-semibold text-gray-500">
-                        ({match.penaltyShoots.localGoals})
-                      </span>
-                    )}
-                    <span
-                      className="font-bold text-2xl text-blue-700 dark:text-blue-500"
-                      role="heading"
-                      aria-level={3}
-                      aria-label={`Goles del equipo local ${match.localTeam.name}`}
-                    >
-                      {match.localScore}
-                    </span>
-                    <span>-</span>
-                    <span
-                      className="font-bold text-2xl text-blue-700 dark:text-blue-500"
-                      aria-label={`Goles del equipo visitante ${match.visitorTeam.name}`}
-                    >
-                      {match.visitorScore}
-                    </span>
-                    {match.penaltyShoots && (
-                      <span className="font-semibold text-gray-500">
-                        ({match.penaltyShoots.visitorGoals})
-                      </span>
-                    )}
+              <Link
+                href={
+                  '/liguilla/encuentro' +
+                  `?tournament=${match.tournament.permalink}` +
+                  `&category=${match.tournament.category}` +
+                  `&local_team=${match.localTeam.permalink}` +
+                  `&visitor_team=${match.visitorTeam.permalink}`
+                }
+              >
+                <div className="flex flex-col gap-5 md:flex-row md:gap-5">
+                  <div className="w-full lg:w-1/2 order-2 lg:order-1">
+                    <MatchMetadata
+                      tournamentName={match.tournament.name}
+                      category={match.tournament.category}
+                      format={match.tournament.format}
+                      place={match.place}
+                      date={match.matchDate}
+                      status={match.status}
+                      round={match.round as ROUND_TYPE}
+                      group={match.group}
+                    />
                   </div>
-                  <Team
-                    imageUrl={match.visitorTeam.imageUrl}
-                    name={match.visitorTeam.name}
-                  />
+                  <div className="w-full lg:w-1/2 grid grid-cols-3 order-1 lg:order-2">
+                    <Team
+                      imageUrl={match.localTeam.imageUrl}
+                      name={match.localTeam.name}
+                    />
+                    <div className="flex justify-center items-center gap-2">
+                      {match.penaltyShoots && (
+                        <span className="font-semibold text-gray-500">
+                          ({match.penaltyShoots.localGoals})
+                        </span>
+                      )}
+                      <span
+                        className="font-bold text-2xl text-blue-700 dark:text-blue-500"
+                        role="heading"
+                        aria-level={3}
+                        aria-label={`Goles del equipo local ${match.localTeam.name}`}
+                      >
+                        {match.localScore}
+                      </span>
+                      <span>-</span>
+                      <span
+                        className="font-bold text-2xl text-blue-700 dark:text-blue-500"
+                        aria-label={`Goles del equipo visitante ${match.visitorTeam.name}`}
+                      >
+                        {match.visitorScore}
+                      </span>
+                      {match.penaltyShoots && (
+                        <span className="font-semibold text-gray-500">
+                          ({match.penaltyShoots.visitorGoals})
+                        </span>
+                      )}
+                    </div>
+                    <Team
+                      imageUrl={match.visitorTeam.imageUrl}
+                      name={match.visitorTeam.name}
+                    />
+                  </div>
                 </div>
-              </div>
+              </Link>
               {((matches.length - 1) !== index) && (
                 <div className="w-full h-0.5 bg-gray-300 my-3" />
               )}
