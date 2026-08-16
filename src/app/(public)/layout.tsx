341851e4cf134f8b0adf0139fd267796a0f177a9
Daniel González Briseño <qbixmex@gmail.com>
2026-08-15 19:15:27 -0600

    Replaced Google Tag Manager components with Next.js GoogleAnalytics
    - Replaced dynamic Google Tag Manager head and body components with the GoogleAnalytics component from @next/third-parties.
    - Added the @next/third-parties dependency.
    - Deleted the dynamic-google-tag-manager-head and dynamic-google-tag-manager-body components.

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -3,9 +3,9 @@ import type { Metadata } from 'next';
 import { Suspense } from 'react';
 import { Container, Footer, Header } from './components';
 import { fetchPublicGlobalSettingsAction } from '../admin/ajustes-globales/(actions)/fetchPublicGlobalSettingsAction';
-import { DynamicGoogleTagManagerHead } from '@/components/dynamic-google-tag-manager-head';
+import { GoogleTagManager } from '@next/third-parties/google';
+import { GoogleAnalytics } from '@next/third-parties/google';
 import '@/app/globals.css';
-import { DynamicGoogleTagManagerBody } from '@/components/dynamic-google-tag-manager-body';
 
 export const generateMetadata = async (): Promise<Metadata> => {
   const { globalSettings } = await fetchPublicGlobalSettingsAction();
@@ -66,10 +66,6 @@ const PublicLayoutContent: FC<Props> = async ({ children }) => {
 
   return (
     <>
-      <DynamicGoogleTagManagerHead
-        googleTagManagerId={globalSettings?.googleTagManager}
-      />
-
       <Container>
         <Header
           siteLogo={globalSettings?.logoUrl ?? null}
@@ -89,10 +85,11 @@ const PublicLayoutContent: FC<Props> = async ({ children }) => {
           ]}
         />
       </Container>
-
-      <DynamicGoogleTagManagerBody
-        googleTagManagerId={globalSettings?.googleTagManager}
-      />
+      {
+        globalSettings?.googleAnalyticsId && (
+          <GoogleAnalytics gaId={globalSettings.googleAnalyticsId} />
+        )
+      }
     </>
   );
 };
